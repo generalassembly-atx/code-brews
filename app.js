3d50a45ab9d7ae59343764d1971a4aca861d509c
Mike Dang <mike.dang@ga.co>
2016-10-16 15:48:15 -0500

    Working API

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -7,6 +9,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var api = require('./routes/api');
 
 var app = express();
 
@@ -24,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/api', api);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/api.js
@@ -0,0 +1,49 @@
+var express = require('express');
+var router = express.Router();
+var axios = require('axios');
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('Invalid API endpoint');
+});
+
+// http://www.brewerydb.com/developers/docs-endpoint/beer_index
+// BreweryDB requires at least one of the following attributes be set:
+//  name, abv, ibu, srmId, availabilityId, styleId
+router.get('/beers', function(req, res, next) {
+  // Send over everything in the query string from Codepen, the only difference is
+  //  we also need to send the 'key' access token
+  const params = Object.assign({
+    key: process.env.BREWERYDB_API_KEY
+  }, req.query);
+  axios.get(`${ process.env.BREWERYDB_API_BASE_URL }beers`, {
+    params: params
+  })
+  .then(function(response) {
+    res.json(response.data);
+  })
+  .catch(function(error) {
+    res.status(500).send(error);
+  });
+});
+
+// http://www.brewerydb.com/developers/docs-endpoint/beer_index
+router.get('/beers/:beerId', function(req, res, next) {
+  // Send over everything in the query string from Codepen, the only difference is
+  //  we also need to send the 'key' access token
+  const params = Object.assign({
+    key: process.env.BREWERYDB_API_KEY
+  }, req.query);
+
+  axios.get(`${ process.env.BREWERYDB_API_BASE_URL }beer/${ req.params.beerId }`, {
+    params: params
+  })
+  .then(function(response) {
+    res.json(response.data);
+  })
+  .catch(function(error) {
+    res.status(500).send(error);
+  });
+});
+
+module.exports = router;
